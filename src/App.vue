dab3cfc95f0e3cad6bcb87f93298f16ba08d8ab2
Sherwin Arellano <s.arellano@esis-italia.com>
2025-04-04 15:32:19 +0200

    Create watchers examples

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
 import ComponentBasics from "@/features/component-basics/ComponentBasics.vue";
+import { ref } from "vue";
+import Watchers from "./features/watchers/Watchers.vue";
+
+const tabs = {
+  ComponentBasics,
+  Watchers,
+};
+
+const currentTab = ref<keyof typeof tabs>("ComponentBasics");
 </script>
 
 <template>
-  <ComponentBasics />
+  <nav>
+    <button
+      v-for="(_, tab) in tabs"
+      type="button"
+      :key="tab"
+      :class="['tab-button', { active: currentTab === tab }]"
+      @click="currentTab = tab"
+    >
+      {{ tab }}
+    </button>
+  </nav>
+
+  <component :is="tabs[currentTab]"></component>
 </template>
 
 <style scoped></style>
---
+++ src/features/watchers/Watchers.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref, watch } from "vue";
+
+const x = ref(0);
+const obj = ref({ count: 0 });
+
+watch(
+  x,
+  (newX) => {
+    console.log(`x is ${x.value}`);
+    console.log(`x is ${newX}`);
+  },
+  {
+    immediate: true,
+  }
+);
+
+watch(
+  () => obj.value.count,
+  (count) => {
+    console.log("Obj Count:", count);
+  }
+);
+</script>
+
+<template>
+  <button @click="x++">Mutate X</button>
+  <button @click="obj.count++">Mutate Obj</button>
+</template>
+
+<style scoped></style>
